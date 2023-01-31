6bd668add4629624535771938330b43b06bd3fa9
Luke Curran <lcurran@canarytechnologies.com>
2023-01-31 14:34:53 -0600

    Support sourcemaps in the build

--- examples/vite-svg-example/vite.config.js
+++ examples/vite-svg-example/vite.config.js
@@ -1,5 +1,8 @@
 const { createSvgPlugin } = require("vite-plugin-vue2-svg");
 const { createVuePlugin } = require("vite-plugin-vue2");
 module.exports = {
+  build: {
+    sourcemap: true,
+  },
   plugins: [createVuePlugin(), createSvgPlugin({})],
 };
